9f99b4fe55b24a01408a58f12472c33953d30e04
vdoroshyn <kjioyh9@gmail.com>
2018-05-21 19:39:36 +0300

    added the first if clause to the main, starting lexer and parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-	std::cout << "Hello World!" << std::endl;
+int main(int argc, char *argv[]) {
+
+	if (argc == 1) {
+		//read stuff from the standard input
+		//end at user's input == ";;"
+		std::cout << argv[0] << std::endl;
+
+	} else if (argc == 2) {
+		//read stuff from file
+		//end at EOF
+		std::cout << argv[1] << std::endl;
+	} else {
+		std::cout << "There are too many arguments" << std::endl;
+	}
+
+
+	// std::vector<int> testVector;
+
+	// testVector.push_back(3);
+	// testVector.push_back(4);
+	// testVector.push_back(5);
+	// std::cout << "Vector: ";
+
+	// for (unsigned int i = 0; i < testVector.size(); ++i) {
+	// 	std::cout << testVector.at(i) << " ";
+	// }
+
+	// std::cout << std::endl;
 	return 0;
 }
