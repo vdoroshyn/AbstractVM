3839062fd88791ed26484e4d6d9b8f2f38869636
vdoroshyn <kjioyh9@gmail.com>
2018-06-11 17:38:29 +0300

    usage seems to be ready

--- Lexer.cpp
+++ Lexer.cpp
@@ -36,23 +36,23 @@ Lexer::~Lexer() {
 }
 
 void Lexer::printOutUsage() {
-	std::cout << "Comments: Comments start with a ';' symbol and finish with a newline. A comment can be either at the start of a line, or after an instruction." << std::endl; 
-	std::cout << "\033[1;91mpush v\033[0m: Pushes the value v at the top of the stack. The value v must have one of the following form:" << std::endl;
-	std::cout << "int8(n) : Creates an 8-bit integer with value n." << std::endl;
-	std::cout << "int16(n) : Creates a 16-bit integer with value n." << std::endl;
-	std::cout << "int32(n) : Creates a 32-bit integer with value n." << std::endl;
-	std::cout << "float(z) : Creates a float with value z." << std::endl;
-	std::cout << "double(z) : Creates a double with value z." << std::endl;
-	std::cout << "pop: Unstacks the value from the top of the stack. If the stack is empty, the program execution must stop with an error." << std::endl;
-	std::cout << "dump: Displays each value of the stack, from the most recent one to the oldest one WITHOUT CHANGING the stack. Each value is separated from the next one by a newline." << std::endl;
-	std::cout << "assert v: Asserts that the value at the top of the stack is equal to the one passed as parameter for this instruction. If it is not the case, the program execution must stop with an error. The value v has the same form that those passed as parameters to the instruction push." << std::endl;
-	std::cout << "add: Unstacks the first two values on the stack, adds them together and stacks the result. If the number of values on the stack is strictly inferior to 2, the program execution must stop with an error." << std::endl;
-	std::cout << "sub: Unstacks the first two values on the stack, subtracts them, then stacks the result. If the number of values on the stack is strictly inferior to 2, the program execution must stop with an error." << std::endl;
-	std::cout << "mul: Unstacks the first two values on the stack, multiplies them, then stacks the result. If the number of values on the stack is strictly inferior to 2, the program execution must stop with an error." << std::endl;
-	std::cout << "div: Unstacks the first two values on the stack, divides them, then stacks the result. If the number of values on the stack is strictly inferior to 2, the program execution must stop with an error. Moreover, if the divisor is equal to 0, the program execution must stop with an error too. Chatting about floating point values is relevant a this point. If you don’t understand why, some will understand. The linked question is an open one, there’s no definitive answer." << std::endl;
-	std::cout << "mod: Unstacks the first two values on the stack, calculates the modulus, then stacks the result. If the number of values on the stack is strictly inferior to 2, the program execution must stop with an error. Moreover, if the divisor is equal to 0, the program execution must stop with an error too. Same note as above about fp values." << std::endl;
-	std::cout << "print: Asserts that the value at the top of the stack is an 8-bit integer. (If not, see the instruction assert), then interprets it as an ASCII value and displays the corresponding character on the standard output." << std::endl;
-	std::cout << "exit: Terminate the execution of the current program. If this instruction does not appears while all others instruction has been processed, the execution must stop with an error." << std::endl;
+	std::cout << "\033[1;95mcomments\033[0m: \033[0;34mComments start with a '\033[1;94m;\033[0;34m' symbol and end with a newline. A comment can be either at the start of a line or after an instruction.\033[0m" << std::endl; 
+	std::cout << "\033[1;95mpush v\033[0m: \033[0;34mPushes the value \033[1;94mv\033[0m \033[0;34mat the top of the stack, which must have one of the following forms:\033[0m" << std::endl;
+	std::cout << "\t\033[1;94mint8(n) : \033[0;34mCreates a 8-bit integer with the value \033[1;94mn\033[0m." << std::endl;
+	std::cout << "\t\033[1;94mint16(n) : \033[0;34mCreates a 16-bit integer with the value \033[1;94mn\033[0m." << std::endl;
+	std::cout << "\t\033[1;94mint32(n) : \033[0;34mCreates a 32-bit integer with the value \033[1;94mn\033[0m." << std::endl;
+	std::cout << "\t\033[1;94mfloat(z) : \033[0;34mCreates a float with the value \033[1;94mz\033[0m." << std::endl;
+	std::cout << "\t\033[1;94mdouble(z) : \033[0;34mCreates a double with the value \033[1;94mz\033[0m." << std::endl << std::endl;
+	std::cout << "\033[1;95mpop\033[0m: \033[0;34mUnstacks the value from the top of the stack. \033[1;91mIf the stack is empty, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mdump\033[0m: \033[0;34mDisplays each value of the stack from the most recent one to the oldest one without changing the stack.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95massert v\033[0m: \033[0;34mAsserts that the value at the top of the stack is equal to the one passed as parameter for this instruction. \033[1;91mIf it is not - the program execution stops with an error.\033[0m \033[0;34mThe value \033[1;94mv\033[0m \033[0;34mhas the same form that those passed as parameters to the command push.\033[0m" << std::endl<< std::endl;
+	std::cout << "\033[1;95madd\033[0m: \033[0;34mUnstacks the first two values in the stack, adds them together, and pushes the result on the stack. \033[1;91mIf there are less than 2 operands in the stack, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95msub\033[0m: \033[0;34mUnstacks the first two values in the stack, subtracts them, and pushes the result on the stack. \033[1;91mIf there are less than 2 operands in the stack, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mmul\033[0m: \033[0;34mUnstacks the first two values in the stack, multiplies them, and pushes the result on the stack. \033[1;91mIf there are less than 2 operands in the stack, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mdiv\033[0m: \033[0;34mUnstacks the first two values in the stack, divides them, and pushes the result on the stack. \033[1;91mIf there are less than 2 operands in the stack or the divisor is equal to 0, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mmod\033[0m: \033[0;34mUnstacks the first two values in the stack, calculates the modulus, and pushes the result on the stack. \033[1;91mIf there are less than 2 operands in the stack or the divisor is equal to 0, the program execution stops with an error.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mprint\033[0m: \033[0;34mAsserts that the value at the top of the stack is a 8-bit integer (\033[1;91mif not, check the instructions for assert\033[0m\033[0;34m); then interprets it as an ASCII value and displays the corresponding character on the standard output.\033[0m" << std::endl << std::endl;
+	std::cout << "\033[1;95mexit\033[0m: \033[0;34mTerminates the execution of the current program. \033[1;91mThere must be only one exit command and it must be located at the end of the input.\033[0m" << std::endl;
 }
 
 void Lexer::readFromFile(char* file) {
